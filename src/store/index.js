3446f8c70cf0687cd04665b8a1630d8d78ba0466
maria.edwards <maria.ana.edwards@gmail.com>
2023-07-04 16:14:23 -0700

    Adds redux-toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,51 @@
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const initialState = {
   counter: 0,
   showCounter: true,
 };
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return { ...state, counter: state.counter + action.payload };
-    case 'DECREMENT':
-      return { ...state, counter: state.counter - action.payload };
-    case 'TOGGLE_COUNTER':
-      return {
-        ...state,
-        showCounter: !state.showCounter,
-      };
-    default:
-      return state;
-  }
-};
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state, action) => {
+      state.counter = state.counter + action.payload;
+    },
+    decrement: (state, action) => {
+      state.counter = state.counter - action.payload;
+    },
+    toggleCounter: (state) => {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-const store = createStore(reducer);
+// pure redux
+// const reducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return { ...state, counter: state.counter + action.payload };
+//     case 'DECREMENT':
+//       return { ...state, counter: state.counter - action.payload };
+//     case 'TOGGLE_COUNTER':
+//       return {
+//         ...state,
+//         showCounter: !state.showCounter,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// pure redux
+// const store = createStore(reducer);
+
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
+
+export const counterActions = counterSlice.actions;
 
 export default store;
